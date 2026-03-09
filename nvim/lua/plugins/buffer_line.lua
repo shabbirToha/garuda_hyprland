875d7a4e1fcd34d92a9eb46ff1ef36135492e8c6
shabbir toha <shabbir.toha@gmail.com>
2026-03-09 11:24:25 +0600

    update nvim config files

--- nvim/lua/plugins/buffer_line.lua
+++ nvim/lua/plugins/buffer_line.lua
@@ -1,16 +1,29 @@
 return {
-  "akinsho/bufferline.nvim",
-  enabled = true,
-  opts = {
-    options = {
-      numbers = "ordinal",
-      modified_icon = "",
-      color_icons = true,
-      separator_style = "thick", -- You can use: "slant", "thick", "thin", etc.
-      show_buffer_close_icons = true,
-      show_close_icon = false,
-      diagnostics = "nvim_lsp",
-      diagnostics_update_in_insert = false,
-    },
-  },
+	"akinsho/bufferline.nvim",
+	enabled = true,
+	opts = {
+		options = {
+			-- Existing
+			numbers = "ordinal",
+			color_icons = true,
+			separator_style = "thick", -- "slant", "thick", "thin", etc.
+			diagnostics = "nvim_lsp",
+			diagnostics_update_in_insert = false,
+
+			-- Added
+			themable = true,
+
+			indicator = {
+				icon = "  ",
+				style = "icon",
+			},
+
+			show_buffer_close_icons = true,
+			show_close_icon = true, -- changed from false since you wanted it added
+
+			buffer_close_icon = "",
+			close_icon = "",
+			modified_icon = "",
+		},
+	},
 }
